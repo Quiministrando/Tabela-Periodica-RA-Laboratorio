27040485cc038500605f420307e3acfbdb93e18a
Quiministrando <rodrigomsfons@gmail.com>
2026-08-22 09:21:38 -0300

    Add files via upload

---
+++ elementos.js
@@ -0,0 +1,16 @@
+window.ELEMENTOS = [
+  {
+    id: "na",
+    nome: "Sódio",
+    simbolo: "Na",
+    numeroAtomico: 11,
+    protons: 11,
+    neutrons: 12,
+    eletrons: 11,
+    numeroMassa: 23,
+    isotopoReferencia: "²³Na",
+    camadas: [2, 8, 1],
+    distribuicao: "2–8–1",
+    configuracaoCondensada: "[Ne] 3s¹"
+  }
+];
